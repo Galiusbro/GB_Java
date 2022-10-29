1e2d59f0c926305df07e0bdb2e3179f9df3403c9
Galiusbro <galiusbro@gmail.com>
2022-10-29 16:11:46 +0300

    Healers rools of the world

---
+++ seminars/finalprojekt.java
@@ -0,0 +1,10 @@
+// 1. Описать класс для работы с бинарным деревом  
+// 1.1 Реализовать три варианта обхода - ru.wikipedia.org/wiki/Обход_дерева#Прямой_обход_(NLR)
+// 1.2 Сделать скобочную запись бинарного дерева
+
+public class finalprojekt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world! Привет мир!");
+    }
+}
